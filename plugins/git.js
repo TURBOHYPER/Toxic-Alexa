a919be0c1a8e8974386dac58182b327738387694
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-03-11 21:14:21 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,9 +7,9 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://telegra.ph/file/f6ec3d3d845a1e3dcce39.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Turbo*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Alexa bot created by Turbo*
 *Creator number : wa.me/916380260672?text=Hi%20Turbo%20Mods.%20*
 
 *Githublink :    coming soon*
