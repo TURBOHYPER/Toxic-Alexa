3c401193aac15c8c0ba63a62c6f39cb3b0f7811e
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-03-12 09:10:33 +0530

    🥵Toxic-Alexa🥶

--- plugins/anime.js
+++ plugins/anime.js
@@ -94,7 +94,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*Made By Toxic-Alexa*'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*Made By  Toxic-Alexa*'})
 
     }));
 }
