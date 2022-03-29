aff8e3d97fe5d7390ed2a1d3eaa14d56f4b1109e
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-03-29 12:07:05 +0530

    🥵Toxic-Alexa🥶

---
+++ plugins/mforward.js
@@ -0,0 +1,44 @@
+/* coded by Turbo
+*/
+
+ const Alexa = require('../events');
+ const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+ const fs = require('fs');
+ const ffmpeg = require('fluent-ffmpeg');
+ const { execFile } = require('child_process');
+ const Config = require('../config');
+
+          var mode = ''
+if (Config.BC == 'default') mode = 'ᴅᴇᴠᴇʟᴏᴘᴇʀ : ī.am/ꪶᴛᴜʀʙᴏꫂ⁩⁩⁩'
+
+else mode = 'ғᴏʀᴡᴀʀᴅᴇᴅ ʙʏ ' + Config.BC
+
+            const turbo = {
+
+                  key: {"fromMe": false,"participant": "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "994403219940-1631515755@g.us","inviteCode": "mememteeeekkeke","groupName": "𝐓𝚯𝚾𝚰𝐂 ₪ 𝚨𝐋𝚵𝚾𝚨  ⷡ  ⷪ  ⷮⷮ 🪀", "caption": mode , 'jpegThumbnail': fs.readFileSync('./photo/Alexa.jpg')}}
+             
+             
+            }
+
+Alexa.addCommand({pattern: 'alexafor ?(.*)', fromMe: true, desc: 'its foraward replied audio'}, (async (message, match) => {   
+  
+    if (message.reply_message === false);
+    
+    
+      
+      
+    var location = await message.client.downloadAndSaveMediaMessage({
+        key: {
+            remoteJid: message.reply_message.jid,
+            id: message.reply_message.id
+        },
+        message: message.reply_message.data.quotedMessage
+    });
+let id = match[1];
+    ffmpeg(location)
+        .format('mp3')
+        .save('output.mp3')
+        .on('end', async () => {
+           await message.client.sendMessage(id, fs.readFileSync('output.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio,duration: Config.SAID, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 9999999999, status: 200, thumbnail: fs.readFileSync('./photo/Alexa.jpg'), surface: 200, message: Config.BC, orderTitle: Config.BC, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOT + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./photo/Alexa.jpg')}}}});
+});
+}));
