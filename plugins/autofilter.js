d4607eebbef3ced75d82ee5dad3ab2ba2f54f0b2
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-03-29 12:06:44 +0530

    🥵Toxic-Alexa🥶

---
+++ plugins/autofilter.js
@@ -0,0 +1,74 @@
+const asena = require('../events');
+
+const {MessageType} = require('@adiwajshing/baileys');
+
+const GM = "."
+
+const GN = "."
+
+const Config = require('../config');
+
+if (Config.WORKTYPE == 'private') {
+
+asena.addCommand({pattern: 'Turbo', fromMe: true, deleteCommand: true, desc: GM,}, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    r_text[1] = "Turbo Is My Creator";
+
+    var i = Math.floor(2*Math.random())
+
+    await message.client.sendMessage(
+
+        message.jid,(r_text[i]), MessageType.text);
+
+    }));
+
+asena.addCommand({pattern: 'Jifi', fromMe: true, deleteCommand: true, desc: GN,}, (async (message, match) => {
+
+        var r_text = new Array ();
+
+        r_text[1] = "Jifi Is My Creator";
+
+        var i = Math.floor(2*Math.random())
+
+        await message.client.sendMessage(
+
+            message.jid,(r_text[i]), MessageType.text);
+
+        }));    
+
+    }
+
+    if (Config.WORKTYPE == 'public') {
+
+        asena.addCommand({pattern: 'Turbo', fromMe: false, deleteCommand: true, desc: GM,}, (async (message, match) => {
+
+            var r_text = new Array ();
+
+            r_text[1] = "Turbo Is My Creator";
+
+            var i = Math.floor(2*Math.random())
+
+            await message.client.sendMessage(
+
+                message.jid,(r_text[i]), MessageType.text);
+
+            }));
+
+        asena.addCommand({pattern: 'Jifi', fromMe: false, deleteCommand: true, desc: GN,}, (async (message, match) => {
+
+                var r_text = new Array ();
+
+                r_text[1] = "Jifi Is My Creator";
+          
+                var i = Math.floor(2*Math.random())
+
+                await message.client.sendMessage(
+
+                    message.jid,(r_text[i]), MessageType.text);
+
+                }));    
+
+
+}
