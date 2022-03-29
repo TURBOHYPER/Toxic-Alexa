ce7621c2824c8cea6163aad9d8fcbc201c2b8ef7
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-03-29 18:00:26 +0530

    🥵Toxic-Alexa🥶

--- plugins/menu.js
+++ plugins/menu.js
@@ -37,7 +37,7 @@ if (config.AUTOBİO == 'true') auto_bio = 'On'
 if (config.AUTOBİO == 'false') auto_bio = 'Off'
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, contextInfo: {mentionedJid: message.mention}, caption: `╭──────────────────╮
-    ` + config.BOT + `
+    ` + config.BOTSK + `
 ╭──────────────────╯
 │
 │ ʜᴇʏ ᴜsᴇʀ ` + wish + `
