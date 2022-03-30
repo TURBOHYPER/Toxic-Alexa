07cf96ad8a96479d76e778137b5587196e408a18
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-03-30 23:28:14 +0530

    🥵Toxic-Alexa🥶

--- plugins/Register.js
+++ plugins/Register.js
@@ -2,8 +2,8 @@ const { createHash } = require('crypto')
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.db.data.users[m.sender]
-  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Format salah\n*${usedPrefix}daftar nama.umur*`
+  if (user.registered === true) throw `You are already registered\nWant to re-register? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Incorrect format\n*${usedPrefix}list of names.age*`
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw 'Name cannot be empty (Alphanumeric)'
   if (!age) throw 'Age cannot be blank (Numbers)'
