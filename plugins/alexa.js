4c40abbf1e88f7028ef492ce00fd4507d4dadf98
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-03-30 21:26:29 +0530

    🥵Toxic-Alexa🥶

--- plugins/alexa.js
+++ plugins/alexa.js
@@ -11,7 +11,7 @@ var ddd = ggg.toString('utf-8')
 
 let whb = Config.WORKTYPE == 'public' ? false : true
 
-Asena.addCommand({pattern: 'maalutty', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'alexa', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!
     var plk_say = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
