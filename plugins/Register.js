cd5c513c0a2924803f847a7650335ed79aaac061
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-03-30 21:02:57 +0530

    🥵Toxic-Alexa🥶

---
+++ plugins/Register.js
@@ -0,0 +1,32 @@
+const { createHash } = require('crypto')
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { text, usedPrefix }) {
+  let user = global.db.data.users[m.sender]
+  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Format salah\n*${usedPrefix}daftar nama.umur*`
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) throw 'Name cannot be empty (Alphanumeric)'
+  if (!age) throw 'Age cannot be blank (Numbers)'
+  age = parseInt(age)
+  if (age > 120) throw 'Age too old 😂'
+  if (age < 5) throw 'Babies can type according to the bjir format ._.'
+  user.name = name.trim()
+  user.age = age
+  user.regTime = + new Date
+  user.registered = true
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  m.reply(`
+Register successfully!
+╭─「 Info 」
+│ Name: ${name}
+│ Age: ${age} year
+│ SN: ${sn}
+╰────
+`.trim())
+}
+handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
+handler.tags = ['exp']
+
+handler.command = /^(daftar|reg(ister)?)$/i
+
+module.exports = handler
