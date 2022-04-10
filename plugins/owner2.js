38ba80ef6fb39672a6d00fc4215f8d5985b1d68e
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-04-10 08:20:44 +0530

    🥵Toxic-Alexa🥶

--- plugins/owner2.js
+++ plugins/owner2.js
@@ -13,7 +13,7 @@ var ddd = ggg.toString('utf-8')
 Turbo.addCommand({pattern: 'owner', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 // send a list message!
     const rows = [
-        {title: '𝙽𝙰𝙼𝙴', description: "🍁 ɴᴀᴍᴇ ᴏғ ᴏᴡɴᴇʀ 🍁.\n\n\nToxic Turbo\n ", rowId:" rowid1"},
+        {title: '𝙽𝙰𝙼𝙴', description: "🍁 ɴᴀᴍᴇ ᴏғ ᴏᴡɴᴇʀ 🍁.\n\n\nToxic-Turbo\n ", rowId:" rowid1"},
         {title: '𝙸𝙽𝚂𝚃𝙰 ', description: "🍁 ɪɴsᴛᴀɢʀᴀᴍ 🍁.\n\n\nhttps://instagram.com/toxic_turbo777\n ", rowId:" rowid2"},
         {title: '𝙽𝚄𝙼𝙱𝙴𝚁', description: "🍁 ɴᴜᴍʙᴇʀ 🍁.\n\n\nhttps://wa.me/916380260672\n ", rowId:" rowid3"},
         ]
