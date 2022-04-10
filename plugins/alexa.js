9d83ce9aa05100f531d4fe95a6c219aedfa704af
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-04-10 08:19:15 +0530

    🥵Toxic-Alexa🥶

--- plugins/alexa.js
+++ plugins/alexa.js
@@ -19,7 +19,7 @@ var plk_here = new Date().toLocaleDateString(get_localized_date)
 var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '```'
 	const buttons = [
 
-        {buttonId: 'id1', buttonText: {displayText: 'PODA😏'}, type: 1},
+        {buttonId: 'id1', buttonText: {displayText: 'PODI😏'}, type: 1},
         {buttonId: 'id2', buttonText: {displayText: 'NJAN MANDAN ONNUMALLA😝'}, type: 1},
         {buttonId: 'id3', buttonText: {displayText: '💝OWNER💝 \n\n```creater Turbo and Jifi```\n\n```owner number wa.me/916380260672```\n\n```owner number wa.me/919995801023```\n\n```whatsapp group : https://chat.whatsapp.com/LWjJ4tu2qe9BWQZ1JzRZgp```\n\n\n'  }, type: 1},
 
