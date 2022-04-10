bcc30ffef5c310f014c02397e2a95c74de9fe698
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-04-10 05:58:31 +0530

    Fix..

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *Alexa*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *Alexa*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *Alexa*:  ```" + msg + "```"
 }
 
 
