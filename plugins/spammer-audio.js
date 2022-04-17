2f04b698ea24bc6292965444d2feda5935157ab6
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-04-17 19:35:29 +0530

    🥵Toxic-Alexa🥶

--- plugins/spammer-audio.js
+++ plugins/spammer-audio.js
@@ -1,4 +1,4 @@
-const AlphaX = require('../events');
+const Turbo = require('../events');
 const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
@@ -8,7 +8,7 @@ const cwebp = require('cwebp-bin');
 const Language = require('../language');
 const Lang = Language.getString('spammer');
 
-AlphaX.addCommand({pattern: 'audio spam$', fromMe: true, desc: Lang.AU_DESC}, (async (message, match) => {
+Turbo.addCommand({pattern: 'audio spam$', fromMe: true, desc: Lang.AU_DESC}, (async (message, match) => {
     
     if (!message.reply_message) return await message.client.sendMessage(message.jid, Lang.AU_REP, MessageType.text);
 
