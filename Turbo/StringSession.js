af132af387ea76dc20c8919d95a6f337eedda1ec
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-20 17:18:14 +0530

    Fix

--- Turbo/StringSession.js
+++ Turbo/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'PIKACHU;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'TOXIC_ALEXA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
